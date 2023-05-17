99b3d7808b9ccc9383099b857f125908b413b3e0
Mega Cat <127934713+Mega-Cat@users.noreply.github.com>
2023-05-17 10:40:25 +0200

    feat: Added a script for deploy into the GTON Testnet

---
+++ scripts/deploy-gton-testnet.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+
+const OGXT_SIM_TSLA_POOL_CONTRACT_ADDR = "0x45581064DE5264f458C59bbF861BA01142cC5b0b";
+const OGXT_TOKEN_ADDR = "0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2";
+const SIM_TSLA_TOKEN_ADDR = "0xBca5De3E6Ea3B39770C0a6EF3793e8fA6424031e";
+
+async function main() {
+  const calibratorFactory = await ethers.getContractFactory("Calibrator");
+  
+  const calibratorContract = await calibratorFactory.deploy(
+    OGXT_SIM_TSLA_POOL_CONTRACT_ADDR,
+    SIM_TSLA_TOKEN_ADDR,
+    OGXT_TOKEN_ADDR,
+  );
+  
+  console.log('"Calibrator" was deployed with:');
+  console.log(`Address: ${calibratorContract.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
